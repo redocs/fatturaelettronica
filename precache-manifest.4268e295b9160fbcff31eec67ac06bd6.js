2063d1c739eae5a8e85c9545ac5bb7b17ceb6e9d
Leonardo Lenzi <leonardo.lenzi@spazionova.it>
2019-01-22 01:39:05 +0100

    Updates

---
+++ precache-manifest.4268e295b9160fbcff31eec67ac06bd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7680e673d930411ccbbf",
+    "url": "/fatturaelettronica/static/css/main.b308bd58.chunk.css"
+  },
+  {
+    "revision": "7680e673d930411ccbbf",
+    "url": "/fatturaelettronica/static/js/main.7680e673.chunk.js"
+  },
+  {
+    "revision": "4ae45b90578ca2d4cdff",
+    "url": "/fatturaelettronica/static/js/1.4ae45b90.chunk.js"
+  },
+  {
+    "revision": "ab3ddb9dcd1a47680188",
+    "url": "/fatturaelettronica/static/js/runtime~main.ab3ddb9d.js"
+  },
+  {
+    "revision": "0f63d51e68a67ad67baeec1df0e4bed6",
+    "url": "/fatturaelettronica/index.html"
+  }
+];
